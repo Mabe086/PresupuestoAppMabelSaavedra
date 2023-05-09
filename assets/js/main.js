666789aea5b8fdd392d5af690254a9f44d3c911c
Mabel Saavedra <mabel.saavedra03@inacapmail.cl>
2023-05-08 22:51:37 -0400

    avance lunes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
-function getValue(buttonValue) {
-    var key = document.getElementById(buttonValue).innerHTML;
-    var screen = document.getElementById("screen").value;
-    document.getElementById('screen').value = screen + key;
-}
+var presupuesto = document.getElementById("presupuesto");
+    presupuesto.style.borderColor = "#145A32";
+
+var calcular = document.getElementById("calcular");
+    calcular.style.backgroundColor = "#D2FFCF";
+    calcular.style.color = "#145A32";
+    calcular.style.borderColor = "#145A32";
+
+var gasto = document.getElementById("gasto");
+    gasto.style.borderColor = "#7B241C";
+
+var anadirGasto = document.getElementById("anadirGasto");
+    anadirGasto.style.backgroundColor = "#FDBAAE";
+    anadirGasto.style.color ="#7B241C";
+    anadirGasto.style.borderColor ="#7B241C";
+
+var montoPresupuesto = document.getElementById("montoPresupuesto")
