2afb69e888bcc058cae31b5a44231356d4a005f1
Mabel Saavedra <mabel.saavedra03@inacapmail.cl>
2023-05-14 18:36:27 -0400

    Presupuesto app Mabel Saavedra

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,4 @@
-var presupuesto = document.getElementById("presupuesto");
-    presupuesto.style.borderColor = "#145A32";
+var presupuesto = []
+var gastos = []
+var listado = document.querySelector("#total-gasto tbody")
 
-var calcular = document.getElementById("calcular");
-    calcular.style.backgroundColor = "#D2FFCF";
-    calcular.style.color = "#145A32";
-    calcular.style.borderColor = "#145A32";
-
-var gasto = document.getElementById("gasto");
-    gasto.style.borderColor = "#7B241C";
-
-var anadirGasto = document.getElementById("anadirGasto");
-    anadirGasto.style.backgroundColor = "#FDBAAE";
-    anadirGasto.style.color ="#7B241C";
-    anadirGasto.style.borderColor ="#7B241C";
-
-var montoPresupuesto = document.getElementById("montoPresupuesto")
